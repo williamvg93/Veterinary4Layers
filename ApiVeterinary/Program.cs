893651fd004982897309c5d15915d7151fe8894a
williamvg93 <88460050+williamvg93@users.noreply.github.com>
2023-11-07 12:15:10 +0000

    feat: :sparkles: AddDbContext, Added Connection to DB

--- ApiVeterinary/Program.cs
+++ ApiVeterinary/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+/* Add Connection to DB */
+builder.Services.AddDbContext<VeterinaryContext>(options =>
+{
+    string conectionStrings = builder.Configuration.GetConnectionString("MysqlConnect");
+    options.UseMySql(conectionStrings, ServerVersion.AutoDetect(conectionStrings));
+});
 
 var app = builder.Build();
 
